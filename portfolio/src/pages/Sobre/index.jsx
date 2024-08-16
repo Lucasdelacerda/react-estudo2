0e6f275079c31438e82c8e8dd223449898a28968
Lucas de Lacerda <106289445+Lucasdelacerda@users.noreply.github.com>
2024-08-16 23:01:51 +0000

    aula 5 e 6

--- portfolio/src/pages/Sobre/index.jsx
+++ portfolio/src/pages/Sobre/index.jsx
@@ -1,9 +1,16 @@
 import style from './Sobre.module.css'
+import Header from '../../components/Header'
+import Container from '../../components/Container'
+import Footer from '../../components/Footer'
 
 export default function Sobre() {
     return (
         <div className={style.sobre}>
-
+            <Header/>
+            <Container>
+                
+            </Container> 
+            <Footer/>
         </div>
     )
 }
